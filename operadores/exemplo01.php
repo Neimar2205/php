18e7f75e1a3631c96c6e461cfe2258a24f56ded7
Neimar2205 <neimar.jdlg@gmail.com>
2020-10-23 20:12:35 -0300

    atualização com operadores basicos

---
+++ operadores/exemplo01.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Operador '=' faz a atribuição de valor a uma variavel.
+$nome = "hcode";
+//o ponto é operador de concatenação
+echo $nome  ." mais alguma coisa. <br>";
+
+$nome .= " Treinamentos";
+
+echo $nome;
+
+?> 
