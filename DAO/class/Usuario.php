e102f6b7883dc0849b10638843c115bc2f30dc57
Neimar2205 <neimar.jdlg@gmail.com>
2021-04-25 19:02:55 -0300

    buscar usuario autenticado/logado em um sistema

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -28,7 +28,8 @@ class Usuario {
 		// Aqui o Sql "SELECT * FROM tb_usuarios WHERE idusuario = :ID" e o paramentro $rawQuery
 		// e o "array(":ID"=$id)" e o $params = array() do metodo 'public function select' da class Sql-responsalvel por acessar o DB. 
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
-		//verifica se existe pelo menos 1 registro no retorno.		
+		//verifica se existe pelo menos 1 registro no retorno.
+	
 		if(isset($results[0])){
 		// if (count($results)> 0)  //outra forma de verificar se tem ao menos um resultado
 		$row = $results[0];
@@ -39,7 +40,43 @@ class Usuario {
 		// $this->setDtcadastro(new DateTime($row['dtcadastro'])); Altera o format da data retornada do DB.;
 		}
 	}
-	
+
+	public static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	//// Busca uma lista de usuarios pesquisada pelo login, onde o LIKE(sememlhante)  define os paramentros  de busca por semelhança.
+	public static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH' => "%".$login."%"));
+	}
+
+	//// Busca na tabela "Usuarios" o usuario correspondente ao login definido como parametros
+		public function login($login, $password){		
+			$sql = new Sql();
+			//var_dump($sql);
+			$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN  AND dessenha = :PASSWORD", array(
+				":LOGIN"=>$login,
+				":PASSWORD"=>$password));
+
+				 if(count($results) > 0){
+					// if (count($results)> 0)  //outra forma de verificar se tem ao menos um resultado
+					$row = $results[0];
+					$this->setIdusuario ($row['idusuario']);
+					$this->setDeslogin  ($row['deslogin']);
+					$this->setDessenha  ($row['dessenha']);
+					$this->setDtcadastro($row['dtcadastro']);
+					// $this->setDtcadastro(new DateTime($row['dtcadastro'])); //Altera o format da data retornada do DB.;
+					}  else{
+							throw new Exception("Login e/ou senha invalidos");
+						}
+ 
+		}
+
+
+
 		public function __toString(){
 			return json_encode(array("idusuario"   =>$this->getIdusuario(),
 									 "deslogin"   =>$this->getDeslogin(),
--- DAO/index.php
+++ DAO/index.php
@@ -8,9 +8,24 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios)
  */
 
+/* 
  // Aqui faz o select de um cadastro na tabela tbusuarios passando o ID
 $usuario = new Usuario();
 $usuario->loadById(5);
-echo $usuario;
+echo $usuario; */
+
+//    // Carrega lista de todos os usurios da tabela
+//$listaUsuarios = Usuario::getList();
+//echo json_encode($listaUsuarios);
+
+//// Carrega uma lista de usuarios pesquisada pelo login
+//$buscaUsuario = Usuario::search("ar");
+//echo json_encode($buscaUsuario);
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Lala","1345");
+ echo $usuario;
+
 
 ?>
