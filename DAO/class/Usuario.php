da3862fd05300007bbefe0dc65ee48a20f48d496
Neimar2205 <neimar.jdlg@gmail.com>
2021-04-25 22:48:09 -0300

    Metodo inserte com uso de construtor definido e default. Metodo setDatas() para faser a recuperaço ds dados.

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -7,6 +7,11 @@ class Usuario {
 	private $dessenha;
 	private $dtcadastro;
 
+	public function __construct($login = "", $password = ""){
+		$this->setDeslogin  ($login);
+		$this->setDessenha  ($password);		
+	}
+
 	public function getIdusuario(){ return $this-> idusuario; }
 	public function setIdusuario($value){ $this->idusuario  = $value; }
 
@@ -19,6 +24,14 @@ class Usuario {
 	public function getDtcadastro() { return $this-> dtcadastro;	}
 	public function setDtcadastro($value) { $this->dtcadastro  = $value; }
 
+	public function setDatas($data){
+		$this->setIdusuario ($data['idusuario']);
+		$this->setDeslogin  ($data['deslogin']);
+		$this->setDessenha  ($data['dessenha']);
+		$this->setDtcadastro($data['dtcadastro']);
+		// $this->setDtcadastro(new DateTime($data['dtcadastro'])); Altera o format da data retornada do DB.;
+	}
+
 
 	public function loadById($id){
 		//Cria a instancia/objeto SQl responsvel por realizar as operaçoes no DB
@@ -28,16 +41,12 @@ class Usuario {
 		// Aqui o Sql "SELECT * FROM tb_usuarios WHERE idusuario = :ID" e o paramentro $rawQuery
 		// e o "array(":ID"=$id)" e o $params = array() do metodo 'public function select' da class Sql-responsalvel por acessar o DB. 
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
-		//verifica se existe pelo menos 1 registro no retorno.
+		//verifica se existe pelo menos 1 registro no retorno. 
 	
 		if(isset($results[0])){
 		// if (count($results)> 0)  //outra forma de verificar se tem ao menos um resultado
-		$row = $results[0];
-		$this->setIdusuario ($row['idusuario']);
-		$this->setDeslogin  ($row['deslogin']);
-		$this->setDessenha  ($row['dessenha']);
-		$this->setDtcadastro($row['dtcadastro']);
-		// $this->setDtcadastro(new DateTime($row['dtcadastro'])); Altera o format da data retornada do DB.;
+		$this->setDatas($results[0]);
+
 		}
 	}
 
@@ -53,29 +62,31 @@ class Usuario {
 			':SEARCH' => "%".$login."%"));
 	}
 
-	//// Busca na tabela "Usuarios" o usuario correspondente ao login definido como parametros
+	//// Busca na tabela "Usuarios" o usuario logado/autenticado correspondente ao login definido como parametros
 		public function login($login, $password){		
 			$sql = new Sql();
 			//var_dump($sql);
 			$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN  AND dessenha = :PASSWORD", array(
 				":LOGIN"=>$login,
 				":PASSWORD"=>$password));
-
-				 if(count($results) > 0){
+				
 					// if (count($results)> 0)  //outra forma de verificar se tem ao menos um resultado
-					$row = $results[0];
-					$this->setIdusuario ($row['idusuario']);
-					$this->setDeslogin  ($row['deslogin']);
-					$this->setDessenha  ($row['dessenha']);
-					$this->setDtcadastro($row['dtcadastro']);
-					// $this->setDtcadastro(new DateTime($row['dtcadastro'])); //Altera o format da data retornada do DB.;
-					}  else{
-							throw new Exception("Login e/ou senha invalidos");
-						}
- 
+			if(count($results) > 0){
+				$this->setDatas($results[0]);
+				}  else{
+						throw new Exception("Login e/ou senha invalidos");
+					}
 		}
 
-
+		public function insert(){
+			$sql = new Sql();
+			$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+				':LOGIN'=>$this->getDeslogin(),
+				':PASSWORD'=>$this->getDessenha()));
+			if(count($results) > 0){
+				$this->setDatas($results[0]);
+		}
+	}
 
 		public function __toString(){
 			return json_encode(array("idusuario"   =>$this->getIdusuario(),
--- DAO/index.php
+++ DAO/index.php
@@ -2,11 +2,15 @@
 use date_format;
 require_once('config.php');
 
+
+
 //Aqui faz o select de toda a tabela
 /* $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios)
  */
+
+
 
 /* 
  // Aqui faz o select de um cadastro na tabela tbusuarios passando o ID
@@ -14,18 +18,31 @@ $usuario = new Usuario();
 $usuario->loadById(5);
 echo $usuario; */
 
+
 //    // Carrega lista de todos os usurios da tabela
 //$listaUsuarios = Usuario::getList();
 //echo json_encode($listaUsuarios);
+
 
 //// Carrega uma lista de usuarios pesquisada pelo login
 //$buscaUsuario = Usuario::search("ar");
 //echo json_encode($buscaUsuario);
 
-//Carrega um usuario usando o login e a senha
-$usuario = new Usuario();
-$usuario->login("Lala","1345");
- echo $usuario;
 
+////Carrega um usuario usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("Lala","1345");
+// echo $usuario;
+
+////Sem uso do construtor definido na classe Usuario. Usa apenas o construtor default.
+//$aluno = new Usuario;
+//$aluno->setDeslogin("aluno1");
+//$aluno->setDessenha("senhaAluno1");
+//$aluno->insert();
+
+// Usando o metodo construtor definido na classe usuario.
+$aluno = new Usuario("aluno1", "senhaAluno1");
+$aluno->insert();
+echo $aluno;
 
 ?>
