c803eeb6d0e16c513c990baafc75bdf4103c3a34
Neimar2205 <neimar.jdlg@gmail.com>
2021-05-29 07:15:10 -0300

    Manipular o Dounload de arquivos

---
+++ Downloads/exemplo01.php
@@ -0,0 +1,34 @@
+<?php
+ // aqui guarda o link da image
+ $link = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png";
+
+ //aqui pega o conteudo da imagen apontada pelo link
+ $content = file_get_contents($link);
+
+ //var_dump($content);
+
+ // Aqui ocorre a iterpretaçao do conteudo. 
+ //Transforma o conteudo em um arquivo real(define o tamanho, tipo, tipo de imagem)
+ $parse = parse_url($link);
+
+ //var_dump($parse);
+
+ //
+ echo "</br>";
+ //var_dump(basename($parse["path"]));
+
+//pega somente o nome do arquivo dentro do path do arquivo
+ $basename = basename($parse["path"]);
+
+ //cria a copia ou gera o arquivo do conteudo baixado no disco.
+ $file = fopen($basename, "w+");
+
+ //Escrever dentro do arquivo // reconstroi o arquivo em binario.
+ fwrite($file, $content);
+
+ //fecha o arquivo
+ fclose($file);
+
+?>
+
+<img src="<?=$basename?>">
--- upload/exemplo01.php
+++ upload/exemplo01.php
@@ -9,12 +9,12 @@
 
 <?php
 
-  //$_SERVER guarda o tipo do metodo da request. 
+  //$_SERVER guarda o tipo do método da request. 
   //Aqui o if() Verificar se a chamada  do tipo POST
   if($_SERVER["REQUEST_METHOD"]==="POST"){
      
-    //Aqui e guardado as informaçoes do arquivo que sera recebido: name, tamanho, tipo, etc.
-    //A variavel $_FILES e a responsavel por receber o arquivo enviado.
+    //Aqui e guardado as informações do arquivo que sera recebido: name, tamanho, tipo, etc.
+    //A variável $_FILES e a responsável por receber o arquivo enviado.
     $file = $_FILES["fileUpload"];
 
     // Verifica se ocorreu algum erro no upload do arquivo e gera um exception
@@ -22,7 +22,7 @@
       throw new Exception("Error: ".$file["error"]);
     }
 
-    //Aqui e criado o diretorio onde sera guardado os arquivos subidos. Se ele ja existir ele apenas sob o arquivo
+    //Aqui e criado o diretório onde sera guardado os arquivos subidos. Se ele ja existir ele apenas sob o arquivo
     $dirUploads = "uploads";
 
     if(!is_dir($dirUploads)){
@@ -31,11 +31,11 @@
     }
 
     /// Aqui e onde o upload de fato e feito.
-    //o move_uploaded_file realiza a mudança do arquivo do temp para o diretrio "uploads" 
+    //o move_uploaded_file realiza a mudança do arquivo do temp para o diretório "uploads" 
      if(move_uploaded_file($file["tmp_name"], $dirUploads.DIRECTORY_SEPARATOR .$file["name"])){
             echo "Upload realizado com sucesso";
      }else{
-       throw new Exception("Nao foi possivel realizar o upload.");
+       throw new Exception("Nao foi possível realizar o upload.");
 
 
      }
