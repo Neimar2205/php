340598534f1e4e74bc118f210f15a2fb1a2f253d
Neimar2205 <neimar.jdlg@gmail.com>
2021-07-06 21:57:32 -0300

    Aula  86 gerenciador de dependencia composer

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '53cc7865c9711397f109c93d7f0fdd1cdbbd3b4e',
+    'name' => 'cursohcode/php7',
+  ),
+  'versions' => 
+  array (
+    'cursohcode/php7' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '53cc7865c9711397f109c93d7f0fdd1cdbbd3b4e',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+  ),
+);
