616d7b1c14d596ca6d6b8c4397290df0fe2c482d
Neimar2205 <neimar.jdlg@gmail.com>
2021-07-29 18:14:53 -0300

    Ate aula 88 de templae

---
+++ RainTPL/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '340598534f1e4e74bc118f210f15a2fb1a2f253d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '340598534f1e4e74bc118f210f15a2fb1a2f253d',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'edee683bf242f40cc75bee46a78759f6c1589dca',
+    ),
+  ),
+);
