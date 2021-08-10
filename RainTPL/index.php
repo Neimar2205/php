b46523facdc77533943c261e45a8af3accbea48a
Neimar2205 <30841197+Neimar2205@users.noreply.github.com>
2021-08-10 20:11:56 -0300

    Update index.php

--- RainTPL/index.php
+++ RainTPL/index.php
@@ -69,7 +69,7 @@ $mail->SMTPAuth = true;
 $mail->Username = "neimar.jdlg@gmail.com";
 
 //Password to use for SMTP authentication
-$mail->Password = "G.ns*3891";
+$mail->Password = "";
 
 //Set who the message is to be sent from
 //Note that with gmail you can only use your account address (same as `Username`)
