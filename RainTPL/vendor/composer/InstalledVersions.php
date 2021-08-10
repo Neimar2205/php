0b261fa869bdb66a695752599faddb79081204dd
Neimar2205 <neimar.jdlg@gmail.com>
2021-08-10 20:07:22 -0300

    php Mailler

--- RainTPL/vendor/composer/InstalledVersions.php
+++ RainTPL/vendor/composer/InstalledVersions.php
@@ -30,7 +30,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '340598534f1e4e74bc118f210f15a2fb1a2f253d',
+    'reference' => '3e0a9732cf3fe1f925f066cbd9cb17757a2e6cb7',
     'name' => '__root__',
   ),
   'versions' => 
@@ -42,7 +42,16 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '340598534f1e4e74bc118f210f15a2fb1a2f253d',
+      'reference' => '3e0a9732cf3fe1f925f066cbd9cb17757a2e6cb7',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.0',
+      'version' => '6.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
     ),
     'rain/raintpl' => 
     array (
--- RainTPL/vendor/composer/installed.php
+++ RainTPL/vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '340598534f1e4e74bc118f210f15a2fb1a2f253d',
+    'reference' => '3e0a9732cf3fe1f925f066cbd9cb17757a2e6cb7',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,16 @@
       'aliases' => 
       array (
       ),
-      'reference' => '340598534f1e4e74bc118f210f15a2fb1a2f253d',
+      'reference' => '3e0a9732cf3fe1f925f066cbd9cb17757a2e6cb7',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.0',
+      'version' => '6.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a5b5c43e50b7fba655f793ad27303cd74c57363c',
     ),
     'rain/raintpl' => 
     array (
